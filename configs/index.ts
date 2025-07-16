ea261f11adc6a5c43d666304251a632252e240f1
xorostar <aswadali.me@gmail.com>
2025-07-16 18:01:24 +0500

    default api url to stage

--- configs/index.ts
+++ configs/index.ts
@@ -1,5 +1,6 @@
 export const config = {
   apiBaseUrl:
-    process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api/v1",
+    process.env.NUXT_PUBLIC_API_BASE_URL ||
+    "https://korporatio-api-production.up.railway.app/api/v1",
   // Add more environment variables here as needed
 };
